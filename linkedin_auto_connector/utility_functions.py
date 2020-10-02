7fe6ba600b334dfad5021ee9f2ef332fe28c068c
truant-on-the-couch <helaughshecries@protonmail.ch>
2020-10-01 21:42:41 -0400

    round2 feature implemented

--- linkedin_auto_connector/utility_functions.py
+++ linkedin_auto_connector/utility_functions.py
@@ -4,30 +4,9 @@ from box import Box
 
 
 def get_and_send_msg():
-    # go to excel to get first name
-    # pyautogui.click(35, 200)
-    # time.sleep(1)
-    # left 9 times in this case
-    # pyautogui.press('left', presses=9, interval=.5)
-    # time.sleep(.5)
-    # copy cell
-    # pyautogui.hotkey('ctrl', 'c')
-    # time.sleep(.5)
-    # go back to full name
-    # pyautogui.press('right', presses=9, interval=.5)
-    # time.sleep(.5)
     # go to writer
     pyautogui.click(33, 333)
     time.sleep(1)
-    # double click name
-    # pyautogui.doubleClick(364, 294)
-    # time.sleep(1)
-    # paste name
-    # pyautogui.hotkey('ctrl', 'v')
-    # time.sleep(1)
-    # backspace to fix \n problem
-    # pyautogui.press('backspace')
-    # time.sleep(.5)
     # select all
     pyautogui.hotkey('ctrl', 'a')
     time.sleep(.5)
@@ -58,31 +37,40 @@ def log_person(second_round=bool):
     # copy name
     pyautogui.hotkey('ctrl', 'c')
     time.sleep(1)
-    if second_round=True:
-        #go to ....
-        pyautogui.click()
-        
-    # go to text editor
-    pyautogui.click(35, 270)
-    time.sleep(1)
-    # make a new line on every paste in .txt
-    pyautogui.hotkey('ctrl', 'v')
-    time.sleep(.5)
-    # make a new line
-    pyautogui.press('enter')
-    time.sleep(.5)
-    # save
-    pyautogui.hotkey('ctrl', 's')
-    time.sleep(.5)
+    if second_round is True:
+        # go to ....
+        pyautogui.click(32, 404)
+        time.sleep(1)
+        # paste
+        pyautogui.hotkey('ctrl', 'v')
+        time.sleep(.5)
+        pyautogui.press('enter')
+        time.sleep(.5)
+        # save
+        pyautogui.hotkey('ctrl', 's')
+    else:
+        # go to text editor
+        pyautogui.click(35, 270)
+        time.sleep(1)
+        # make a new line on every paste in .txt
+        pyautogui.hotkey('ctrl', 'v')
+        time.sleep(.5)
+        # make a new line
+        pyautogui.press('enter')
+        time.sleep(.5)
+        # save
+        pyautogui.hotkey('ctrl', 's')
+        time.sleep(.5)
 
 
-def handle_email_verif():
+def handle_email_verif(second_round=bool):
     # if email verif box
     try:
         x, y = pyautogui.locateCenterOnScreen(
             './images/email_verif_box.png', confidence=.5, grayscale=True)
         pyautogui.press('esc')
-        log_person()
+        log_person(second_round=True) if second_round is True else log_person()
+
     except TypeError:
         # still need to click add a note button
         pyautogui.click(820, 340)
@@ -90,7 +78,7 @@ def handle_email_verif():
         get_and_send_msg()
 
 
-def check_if_done_button():
+def check_if_done_button(second_round=bool):
     try:
         # did we pull up the box with done button?
         x1, y1 = pyautogui.locateCenterOnScreen(
@@ -98,4 +86,4 @@ def check_if_done_button():
         # if yes run handle_email_verif()
         handle_email_verif()
     except:
-        log_person()
+        log_person(second_round=True) if second_round is True else log_person()
